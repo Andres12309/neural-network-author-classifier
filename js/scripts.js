a728afe6951e05c7e0c5191efb94b15d3efb8242
Andres12309 <andrius12309@gmail.com>
2021-12-13 22:23:20 -0500

    all

--- js/scripts.js
+++ js/scripts.js
@@ -20,7 +20,18 @@ function train(data) {
     trainedNet = net.toFunction();
 };
 
+
+var tweet = null;
+
+//submit
+function sub() {
+    tweet = document.getElementById("prod").value;
+    console.log(tweet)
+    console.log(execute(tweet));
+};
+
 function execute(input) {
+    var result = null;
     let results = trainedNet(encode(input));
     console.log(results)
     let output;
@@ -28,13 +39,15 @@ function execute(input) {
     if (results.trump > results.kardashian) {
         output = 'Donald Trump'
         certainty = Math.floor(results.trump * 100)
-    } else { 
+    } else {
         output = 'Kim Kardashian'
         certainty = Math.floor(results.kardashian * 100)
     }
 
+    result = "I'm " + certainty + "% sure that tweet was written by " + output;
+    document.getElementById("resultado").setAttribute("value",result)
     return "I'm " + certainty + "% sure that tweet was written by " + output;
 }
 
 train(trainingData);
-console.log(execute("Paste your tweet here"));
+
